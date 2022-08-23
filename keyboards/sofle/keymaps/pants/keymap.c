0f0d3b3ca191c41a4317c883005ebf54a523d729
Samuel Bourasseau <samuel.bourasseau@gmail.com>
2022-08-23 23:36:56 +0200

    Add key override for delete.

--- keyboards/sofle/keymaps/pants/keymap.c
+++ keyboards/sofle/keymaps/pants/keymap.c
@@ -23,8 +23,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,  KC_6, KC_7, KC_8, KC_9, KC_0, KC_BACKSPACE,
-    KC_EQUAL, KC_Q, KC_W, KC_E, KC_R, KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RIGHT_BRACKET,
-    KC_MINUS, KC_A, KC_S, KC_D, KC_F, KC_G,  KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_LEFT_BRACKET,
+    KC_EQUAL, KC_Q, KC_W, KC_E, KC_R, KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET,
+    KC_MINUS, KC_A, KC_S, KC_D, KC_F, KC_G,  KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RIGHT_BRACKET,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,  KC_MUTE, KC_NO,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
     KC_TAB, KC_LALT, KC_LCTRL, KC_SPC, MO(_LTRIGGER),  MO(_RTRIGGER), KC_ENTER, KC_RCTRL, KC_RALT, KC_RWIN
 ),
@@ -39,24 +39,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RTRIGGER] = LAYOUT(
 #if 1
-    KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
 #else
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 #endif
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_GRAVE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_HOME, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_GRAVE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_END, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 [_LTRIGGER] = LAYOUT(
 #if 1
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+    KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 #else
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 #endif
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_BACKSLASH,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_QUOTE,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BACKSLASH,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_QUOTE,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_LWIN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 )
@@ -175,6 +175,16 @@ static void print_status_narrow(void) {
     oled_write_P(PSTR("\n"), false);
     oled_write_P(PSTR("hella"), false);
 
+    if (IS_LAYER_ON(_LTRIGGER) && IS_LAYER_ON(_RTRIGGER))
+        oled_write_P(PSTR("LT+RT"), false);
+    else if (IS_LAYER_ON(_LTRIGGER))
+        oled_write_ln_P(PSTR("LT"), false);
+    else if (IS_LAYER_ON(_RTRIGGER))
+        oled_write_ln_P(PSTR("RT"), false);
+    else
+        oled_write_ln_P(PSTR(""), false);
+
+#if 0
     switch (get_highest_layer(layer_state)) {
     case _QWERTY:
         oled_write_ln_P(PSTR(""), false);
@@ -188,6 +198,14 @@ static void print_status_narrow(void) {
     default:
         oled_write_P(PSTR("unkwn"), false);
     }
+#endif
+
+#if 0
+    if (IS_LAYER_ON(_FKEYS))
+        oled_write_P(PSTR("FKEYS"), false);
+    else
+        oled_write_P(PSTR("\n"), false);
+#endif
 
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
@@ -212,14 +230,53 @@ bool oled_task_user(void) {
 
 #endif
 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    NULL // Null terminate the array of overrides!
+};
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+#if 0
     case KC_RTRIGGER:
+        if (record->event.pressed)
+        {
+            layer_on(_RTRIGGER);
+            layer_on(_FKEYS);
+            //layer_or((1 << _RTRIGGER) || (1 << _FKEYS));
+        }
+        else
+        {
+            layer_off(_RTRIGGER);
+            if (IS_LAYER_OFF(_LTRIGGER))
+                layer_off(_FKEYS);
+            //layer_and(~((1 << _RTRIGGER) || (IS_LAYER_OFF(_LTRIGGER) ? (1 << _FKEYS) : 0)));
+        }
+
         //MO(_RTRIGGER);
         return false;
     case KC_LTRIGGER:
+        if (record->event.pressed)
+        {
+            layer_on(_LTRIGGER);
+            layer_on(_FKEYS);
+            //layer_or((1 << _LTRIGGER) || (1 << _FKEYS));
+        }
+        else
+        {
+            layer_off(_LTRIGGER);
+            if (IS_LAYER_OFF(_RTRIGGER))
+                layer_off(_FKEYS);
+            //layer_and(~((1 << _LTRIGGER) || (IS_LAYER_OFF(_RTRIGGER) ? (1 << _FKEYS) : 0)));
+        }
+
+
         //MO(_LTRIGGER);
         return false;
+#endif
     }
     return true;
 }
