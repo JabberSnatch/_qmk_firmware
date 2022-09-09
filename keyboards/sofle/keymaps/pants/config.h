510a3cf6d078ea5394ebefc1446031f693f0a858
Samuel Bourasseau <samuel.bourasseau@gmail.com>
2022-09-09 14:55:53 +0200

    Add EE_HANDS define, improve split behaviour.
    Try and optimise build for binary size.

--- keyboards/sofle/keymaps/pants/config.h
+++ keyboards/sofle/keymaps/pants/config.h
@@ -4,3 +4,14 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
+#define EE_HANDS
+#define SPLIT_TRANSPORT_MIRROR
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+
+#ifdef ENCODER_RESOLUTION
+#undef ENCODER_RESOLUTION
+#define ENCODER_RESOLUTION 4
+#endif
